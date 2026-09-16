250b9ab13ba5485e903b123e4dde4933dc06e9de
Jack <devjack050700@gmail.com>
2026-09-16 22:14:00 +0900

    chore: react-navigation ESM 변환용 jest 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  // react-navigation 은 ESM 배포라 변환 대상에 포함
+  transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native|@react-native(-community)?|@react-navigation|react-native-screens|react-native-safe-area-context)'],
 };
