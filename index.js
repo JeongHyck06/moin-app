71b6a9b3920862de9aeb3c68462abf346c1ea528
Jack <devjack050700@gmail.com>
2026-09-17 03:33:28 +0900

    feat: 백그라운드 메시지 핸들러 등록

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { getApps } from '@react-native-firebase/app';
+import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
 import App from './App';
 import { name as appName } from './app.json';
+
+// 백그라운드 핸들러는 앱 등록 전에 걸어야 한다, 표시는 OS 가 하므로 여기서는 resolve 만
+if (getApps().length > 0) {
+  setBackgroundMessageHandler(getMessaging(), async () => {});
+}
 
 AppRegistry.registerComponent(appName, () => App);
