acd0dae851ac4932ec93b882af4202f957bd61fd
Jack <devjack050700@gmail.com>
2026-09-17 22:59:20 +0900

    chore: 카카오 네이티브 앱 키 설정

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@
 //   1. 여기 KAKAO_APP_KEY
 //   2. android/gradle.properties 의 KAKAO_APP_KEY (manifest 스킴과 문자열 리소스가 여기서 나옴)
 //   3. ios/Moin/Info.plist 의 KAKAO_APP_KEY 와 CFBundleURLSchemes 의 "kakao" 뒤
-export const KAKAO_APP_KEY = '';
+export const KAKAO_APP_KEY = 'be457a6a43d30ee3ea5af761acfded4c';
